f52cdfb94c36ac60f6fa8e327e8b06c88a64e87e
vovap <vovaprimachuk@gmail.com>
2021-07-30 15:58:16 +0300

    ui interface, connect static, login form

--- gitScrapy/search/views.py
+++ gitScrapy/search/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def getLogin(request):
+    if request.method == 'POST':
+        username = request.POST['gitLogin']
+        context = {
+            'username': username
+        }
+        print(type(context), context['username'])
+        return render(request, 'search/result.html', context)
+
+    return render(request, 'search/index.html')
